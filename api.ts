9e2cf180b3f2bbe06e211520242146e3bb571a20
damienaltman42 <damien.altman42@gmail.com>
2024-11-26 18:32:16 +0100

    Add files via upload
    test

---
+++ api.ts
@@ -0,0 +1,8 @@
+import { auth } from './api/auth';
+import { roulettes } from './api/roulettes';
+import { codes } from './api/codes';
+import { wins } from './api/wins';
+import { likes } from './api/likes';
+
+export { auth, roulettes, codes, wins, likes };
+// test toto
